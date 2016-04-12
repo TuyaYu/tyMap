0da0eb3dfd73546d83574213057ba95d4893b688
余泽禹 <88916897@qq.com>
2016-04-12 21:35:22 +0800

    Initial Commit

---
+++ tyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tyMap
+//
+//  Created by 余泽禹 on 16/4/12.
+//  Copyright © 2016年 余泽禹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
